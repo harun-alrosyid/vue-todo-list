2401b29390e5767c4eea871d9fd9f415fbfdca63
harun-alrosyid <alrosyid07@gmail.com>
2025-08-14 23:32:24 +0700

    feat: handle input

---
+++ src/components/TodoLists/_components/InputTask.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const todoInput = ref('')
+
+watch(todoInput, () => {
+  todoInput.value
+})
+</script>
+
+<template>
+  <div class="flex gap-4 mb-6">
+    <input
+      class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm flex-1 font-mono b"
+      type="text"
+      placeholder="Add a task"
+      v-model.trim="todoInput"
+    />
+    <button
+      @click="$emit('addTask', todoInput)"
+      class="inline-flex items-center justify-center gap-2 py-2 px-4 whitespace-nowrap rounded-md text-sm font-medium bg-amber-50 text-black"
+    >
+      +
+    </button>
+    {{ todoInput }}
+  </div>
+</template>
