f5017aedaf64eca70ccb71f5d7e06977e10e95b0
harun-alrosyid <alrosyid07@gmail.com>
2025-08-14 23:32:10 +0700

    feat: component list

---
+++ src/components/TodoLists/_components/EmptyTask.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div
+    class="w-full flex items-center justify-center border-dashed rounded-lg border-amber-50 border-2 p-4"
+  >
+    <p class="">No tasks yet. Add one above!</p>
+  </div>
+</template>
---
+++ src/components/TodoLists/_components/ListTasks.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  tasks: {
+    type: Array,
+  },
+})
+</script>
+
+<template>
+  <div
+    class="w-full flex items-center justify-center border-dashed rounded-lg border-amber-50 border-2 p-4"
+  >
+    <div>
+      <ul class="flex flex-col gap-4">
+        <li v-for="task in tasks" :key="task.id">
+          {{ task }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
