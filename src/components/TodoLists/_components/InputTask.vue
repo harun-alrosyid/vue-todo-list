2033bfd119ec2ca7a12a2f1c9ea3c726aa0f644b
harun-alrosyid <alrosyid07@gmail.com>
2025-08-15 09:40:23 +0700

    feat: using gett= san set computed in todo input

--- src/components/TodoLists/_components/InputTask.vue
+++ src/components/TodoLists/_components/InputTask.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const emit = defineEmits(['addTask']) // definisikan event
 const todoInput = ref('')
-
+const res = computed({
+  get() {
+    return todoInput.value
+  },
+  set(value) {
+    todoInput.value = value
+  },
+})
 function handleAddTask() {
-  emit('addTask', { item: todoInput.value, completed: false })
+  emit('addTask', { item: res.value, completed: false })
   todoInput.value = ''
 }
 </script>
@@ -16,11 +23,11 @@ function handleAddTask() {
       class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm flex-1 font-mono b"
       type="text"
       placeholder="Add a task"
-      v-model.trim="todoInput"
+      v-model.trim="res"
     />
     <button
       @click="handleAddTask"
-      :disabled="!todoInput"
+      :disabled="!res"
       class="inline-flex items-center justify-center gap-2 py-2 px-4 whitespace-nowrap rounded-md text-sm font-medium bg-amber-50 text-black disabled:cursor-not-allowed disabled:opacity-50"
     >
       +
